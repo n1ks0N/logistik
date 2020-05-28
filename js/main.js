6a58d6e3a8271d8b3f872480b28827fd21b12f8d
niksON <gromyakov05@mail.ru>
2020-05-28 12:09:36 +0300

    master

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
-    $(".services-rightside__item__text").each((i, n) => {
-        $(n).click(() => {
-            $('.services-rightside__title').text($(n).text());
-        });
+    $('.services-rightside__item__text').click(function () {
+        $('.services-rightside__title').text($(this).text());
+    });
+
+    $('.prod-rightside__item').mouseover(function () {
+        $(this).children().not('.prod-rightside__item__header').addClass('prod-rightside__item__active');
+        $(this).addClass('prod-rightside__item__active-bg');
+        $('.prod-rightside__item__header', this).css('background', 'none');
+    });
+
+    $('.prod-rightside__item').mouseleave(function () {
+        $(this).children().not('.prod-rightside__item__header').removeClass('prod-rightside__item__active');
+        $(this).removeClass('prod-rightside__item__active-bg');
+        $('.prod-rightside__item__header', this).css('background', 'rgba(0, 0, 0, 0.5)');
     });
 });
